632cdb7c3e95800764930c8c329c908dced77030
Matthias Kestenholz <mk@feinheit.ch>
2022-12-01 09:07:20 +0100

    day01

---
+++ day01/solve.py
@@ -0,0 +1,24 @@
+def read():
+    with open("input") as f:
+        lines = f.read().splitlines()
+
+    group = []
+    for line in lines:
+        if line:
+            group.append(int(line))
+        else:
+            yield group
+            group = []
+    if group:
+        yield group
+
+
+def main():
+    groups = list(read())
+    carrying = [sum(group) for group in groups]
+    print(max(carrying))
+
+    print(sum(sorted(carrying, reverse=True)[:3]))
+
+if __name__ == "__main__":
+    main()
