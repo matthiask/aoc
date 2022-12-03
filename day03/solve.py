3d87a9b10112ec30f997b1b9cedacf253229c8f4
Matthias Kestenholz <mk@feinheit.ch>
2022-12-03 11:20:24 +0100

    First part

---
+++ day03/solve.py
@@ -0,0 +1,31 @@
+import string
+
+priorities = {
+    thing: index + 1
+    for index, thing in enumerate(string.ascii_lowercase + string.ascii_uppercase)
+}
+
+def read():
+    with open("input.txt") as f:
+        for line in f:
+            yield line.strip()
+
+
+def compartments(lines):
+    for line in lines:
+        half = len(line) // 2
+        yield set(line[:half]), set(line[half:])
+
+
+def common_priority(compartments):
+    same = list(compartments[0] & compartments[1])
+    assert len(same) == 1
+    return priorities[same[0]]
+
+
+if __name__ == "__main__":
+    # print(read())
+    # print(priorities)
+    # print(list(compartments(read())))
+
+    print(sum(common_priority(pack) for pack in compartments(read())))
