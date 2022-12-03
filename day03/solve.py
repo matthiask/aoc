07ec8ab04512e3512dd7669b41e5a3640b08275e
Matthias Kestenholz <mk@feinheit.ch>
2022-12-03 11:25:03 +0100

    Second part

--- day03/solve.py
+++ day03/solve.py
@@ -23,9 +23,24 @@ def common_priority(compartments):
     return priorities[same[0]]
 
 
+def group_three(lines):
+    while lines:
+        group, lines = lines[:3], lines[3:]
+        yield group
+
+
+def common_priority_group(groups):
+    for group in groups:
+        same = list(set(group[0]) & set(group[1]) & set(group[2]))
+        assert len(same) == 1
+        yield priorities[same[0]]
+
+
 if __name__ == "__main__":
     # print(read())
     # print(priorities)
     # print(list(compartments(read())))
 
     print(sum(common_priority(pack) for pack in compartments(read())))
+
+    print(sum(common_priority_group(group_three(list(read())))))
