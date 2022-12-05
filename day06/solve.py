dc84c7ad5910d3ced43b09e3fa6837fde53439aa
Matthias Kestenholz <mk@feinheit.ch>
2022-12-05 15:36:21 +0100

    day06

---
+++ day06/solve.py
@@ -0,0 +1,13 @@
+def find_distinct_characters_of_length(signal, length):
+    groups = [signal[i:] for i in range(length)]
+    for idx, group in enumerate(zip(*groups)):
+        if len(group) == len(set(group)):
+            return idx + length
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        signal = f.read()
+
+    print(find_distinct_characters_of_length(signal, 4))
+    print(find_distinct_characters_of_length(signal, 14))
