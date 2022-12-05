1d6f673bac883e02e0209c5935ee3885dd92fd47
Matthias Kestenholz <mk@feinheit.ch>
2022-12-05 08:40:25 +0100

    Slightly optimize day05 by converting ops to use zero-indexed stacks

--- day05/solve.py
+++ day05/solve.py
@@ -10,7 +10,7 @@ Op = namedtuple("Op", "count from_ to_")
 """
 Implementation notice:
 
-Because it's easier the uppermost crate of each stack has index 0. 
+Because it's easier the uppermost crate of each stack has index 0.
 """
 
 UPPERMOST = 0
@@ -27,7 +27,9 @@ def parse_stacks(lines):
 def parse_ops(lines):
     for line in lines:
         if match := re.match(r"move (\d+) from (\d+) to (\d+)", line):
-            yield Op(*[int(value) for value in match.groups()])
+            count, from_, to_ = map(int, match.groups())
+            # Zero indexing.
+            yield Op(count, from_ - 1, to_ - 1)
 
 
 def parse():
@@ -38,7 +40,7 @@ def parse():
 
 
 def move_one(stacks, from_, to_):
-    stacks[to_ - 1].insert(UPPERMOST, stacks[from_ - 1].pop(UPPERMOST))
+    stacks[to_].insert(UPPERMOST, stacks[from_].pop(UPPERMOST))
 
 
 def part1():
@@ -52,9 +54,9 @@ def part1():
 def part2():
     stacks, ops = parse()
     for op in ops:
-        from_ = stacks[op.from_ - 1]
-        moving, stacks[op.from_ - 1] = from_[:op.count], from_[op.count:]
-        stacks[op.to_ - 1][0:0] = moving
+        from_ = stacks[op.from_]
+        moving, stacks[op.from_] = from_[:op.count], from_[op.count:]
+        stacks[op.to_][UPPERMOST:UPPERMOST] = moving
     print("".join(stack[UPPERMOST] for stack in stacks))
 
 
