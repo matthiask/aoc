37c843985624e26dd5b3aae3d4455289052627db
Matthias Kestenholz <mk@feinheit.ch>
2022-12-08 10:34:20 +0100

    day08 part2

--- day08/solve.py
+++ day08/solve.py
@@ -1,4 +1,7 @@
+import operator
+from functools import reduce
 from itertools import repeat
+from pprint import pprint
 
 
 def visible_trees(forest, x_iterable, y_iterable):
@@ -24,32 +27,89 @@ def part1(forest):
 
     for x in forest_x_range:
         visible |= visible_trees(
-            forest, 
+            forest,
             repeat(x),
             forest_y_range,
         )
         visible |= visible_trees(
-            forest, 
+            forest,
             repeat(x),
             forest_y_range[::-1],
         )
     for y in forest_y_range:
         visible |= visible_trees(
-            forest, 
+            forest,
             forest_x_range,
             repeat(y),
         )
         visible |= visible_trees(
-            forest, 
+            forest,
             forest_x_range[::-1],
             repeat(y),
         )
     return len(visible)
 
 
-if __name__ == "__main__":
-    from pprint import pprint
+def visibility(forest, x_iterable, y_iterable):
+    xi = iter(x_iterable)
+    yi = iter(y_iterable)
+    starting_height = forest[next(yi)][next(xi)]
+    visibility = 0
+    for x, y in zip(xi, yi):
+        visibility += 1
+        if forest[y][x] >= starting_height:
+            break
+    return visibility
 
+
+def part2(forest):
+    x_max = len(forest[0])
+    y_max = len(forest)
+
+    scores = {}
+
+    for y in range(y_max):
+        for x in range(x_max):
+            score = []
+            # Down
+            score.append(
+                visibility(
+                    forest,
+                    repeat(x),
+                    range(y, y_max),
+                )
+            )
+            # Up
+            score.append(
+                visibility(
+                    forest,
+                    repeat(x),
+                    range(y, -1, -1),
+                )
+            )
+            # Left
+            score.append(
+                visibility(
+                    forest,
+                    range(x, -1, -1),
+                    repeat(y),
+                )
+            )
+            # Right
+            score.append(
+                visibility(
+                    forest,
+                    range(x, x_max),
+                    repeat(y),
+                )
+            )
+            scores[(x, y)] = (reduce(operator.mul, score), score)
+
+    pprint(scores)
+    return max(scores.values())
+
+
+if __name__ == "__main__":
     with open("input.txt") as f:
         forest = [list(map(int, list(line.strip()))) for line in f]
 
@@ -57,3 +117,4 @@ if __name__ == "__main__":
     # pprint((forest_x, forest_y))
 
     pprint(part1(forest))
+    pprint(part2(forest))
