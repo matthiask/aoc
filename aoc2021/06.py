0ae124d7185f0524a991e3305bfec19999cf2ee1
Matthias Kestenholz <mk@feinheit.ch>
2022-12-11 13:16:38 +0100

    I needed some help for this one

--- aoc2021/06.py
+++ aoc2021/06.py
@@ -6,22 +6,47 @@ with open("06.txt") as f:
     INITIAL = [int(days) for days in f.read().strip().split(",")]
 
 
-def cycle_one(fish):
-    if fish == 0:
-        return [6, 8]
-    else:
-        return [fish - 1]
+# def cycle_one(fish):
+#     if fish == 0:
+#         return [6, 8]
+#     else:
+#         return [fish - 1]
+#
+#
+# def cycle_all(fishes):
+#     return list(chain.from_iterable(cycle_one(fish) for fish in fishes))
+#
+#
+# def part1():
+#     fishes = INITIAL
+#     for _i in range(80):
+#         fishes = cycle_all(fishes)
+#     print("part1:", len(fishes))
+#
+#
+# part1()
 
 
-def cycle_all(fishes):
-    return list(chain.from_iterable(cycle_one(fish) for fish in fishes))
+counter = Counter()
+counter.update(INITIAL)
+
+generation = [counter.get(days, 0) for days in range(0, 9)]
+
+def cycle(generation):
+    head, *tail = generation
+    tail[6] += head
+    tail.append(head)
+    return tail
 
 
-def part1():
-    fishes = INITIAL
-    for _i in range(80):
-        fishes = cycle_all(fishes)
-    print("part1:", len(fishes))
+p1 = generation[:]
+for _i in range(80):
+    p1 = cycle(p1)
 
+print(sum(p1))
 
-part1()
+p2 = generation[:]
+for _i in range(256):
+    p2 = cycle(p2)
+
+print(sum(p2))
