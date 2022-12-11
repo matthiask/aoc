2687f23599eeea2a2e3c0964f7aeb9180069c631
Matthias Kestenholz <mk@feinheit.ch>
2022-12-11 15:30:20 +0100

    Oh wow I suck

---
+++ aoc2021/11.py
@@ -0,0 +1,60 @@
+import operator
+from functools import reduce
+
+
+with open("11.txt") as f:
+    INITIAL = {
+        (x, y): int(h) for y, line in enumerate(f) for x, h in enumerate(line.strip())
+    }
+
+
+max_x = max(x for x, y in INITIAL.keys()) + 1
+max_y = max(y for x, y in INITIAL.keys()) + 1
+
+all_points = [(x, y) for x in range(max_x) for y in range(max_y)]
+
+
+def pretty(energy):
+    return "\n".join(
+        " ".join(str(energy[(x, y)]) for x in range(max_x))
+        for y in range(max_y)
+    )
+
+
+def surrounding(point):
+    x, y = point
+    deltas = [(1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
+    points = [(x + dx, y + dy) for dx, dy in deltas]
+    return set((x, y) for (x, y) in points if 0 <= x < max_x and 0 <= y < max_y)
+
+
+def enough_energy_to_flash(energy):
+    return {point for point in all_points if energy[point] > 9}
+
+
+def part1():
+    energy = dict(INITIAL)
+    flash_count = 0
+
+    def _maybe_flash(flashed):
+        if new := enough_energy_to_flash(energy) - flashed:
+            for flashing in new:
+                for point in surrounding(flashing):
+                    energy[point] += 1
+
+            return len(new) + _maybe_flash(flashed | new)
+        for point in flashed:
+            energy[point] = 0
+        return 0
+
+    for _i in range(100):
+        for point in all_points:
+            energy[point] += 1
+
+        flash_count += _maybe_flash(set())
+
+    print("part1", flash_count)
+
+
+if __name__ == "__main__":
+    part1()
