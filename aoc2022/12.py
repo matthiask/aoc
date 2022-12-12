dfc728c602151b5660d5374908094d41c38e8582
Matthias Kestenholz <mk@feinheit.ch>
2022-12-12 08:36:03 +0100

    2022 12

---
+++ aoc2022/12.py
@@ -0,0 +1,83 @@
+import heapq
+
+
+with open("12.txt") as f:
+    heightmap = {
+        (x, y): h for y, line in enumerate(f) for x, h in enumerate(line.strip())
+    }
+
+
+start = next(point for point, height in heightmap.items() if height == "S")
+end = next(point for point, height in heightmap.items() if height == "E")
+
+
+max_x = max(x for x, y in heightmap)
+max_y = max(y for x, y in heightmap)
+
+
+def surrounding(point):
+    x, y = point
+    return set(
+        filter(
+            None,
+            (
+                # West
+                (x + 1, y) if x + 1 < max_x else None,
+                # South
+                (x, y + 1) if y + 1 < max_y else None,
+                # East
+                (x - 1, y) if x > 0 else None,
+                # North
+                (x, y - 1) if y > 0 else None,
+            ),
+        )
+    )
+
+
+specials = {
+    "S": "a",
+    "E": "z",
+}
+
+
+def only_visitable(height, points):
+    """
+    >>> only_visitable("a", {(1, 0), (2, 0), (3, 0)})
+    {(1, 0), (2, 0)}
+    """
+    h = ord(specials.get(height, height))
+    # diff = {point: ord(heightmap[point]) - h for point in points}
+    # print(diff)
+    return {point for point in points if ord(specials.get(heightmap[point], heightmap[point])) - h  <= 1}
+
+
+def solve(start):
+    seen = {start}
+    heap = [(0, start)]
+
+    while heap:
+        cost, point = heapq.heappop(heap)
+
+        if point == end:
+            return cost
+
+        for next in only_visitable(heightmap[point], surrounding(point)):
+            if next in seen:
+                continue
+            seen.add(next)
+            heapq.heappush(heap, (cost + 1, next))
+
+        # print(len(heap))
+
+
+def part2():
+    steps = set()
+    for point, height in heightmap.items():
+        if height in {"a", "S"}:
+            steps.add(solve(point) or 99999)
+    return min(steps)
+
+
+if __name__ == "__main__":
+    print("part1", solve(start))
+    print("part2", part2())
