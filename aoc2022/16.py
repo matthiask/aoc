957b4a688038768684b73b71309be9cf12807ffe
Matthias Kestenholz <mk@feinheit.ch>
2022-12-16 11:24:50 +0100

    2022 16 part2

--- aoc2022/16.py
+++ aoc2022/16.py
@@ -56,7 +56,7 @@ def _valve(valves, tunnel_costs, name, open_valves, release, minutes):
     next_open_valves = open_valves + (name,)
     next_release = release + minutes * valves[name].flow_rate
     # print(next_release, sorted(next_open_valves))
-    yield next_release
+    yield next_release, set(next_open_valves)
 
     for to, costs in tunnel_costs[name]:
         if to not in open_valves:
@@ -70,12 +70,33 @@ def _valve(valves, tunnel_costs, name, open_valves, release, minutes):
             )
 
 
-def part1(valves, start, minutes):
+def part1(valves):
     tunnel_costs = determine_tunnel_costs(valves)
-    max_release = set(_valve(valves, tunnel_costs, start, (), 0, 30))
+    max_release = {
+        release
+        for release, open_valves in _valve(valves, tunnel_costs, "AA", (), 0, 30)
+    }
 
     # pprint(max_release)
     return max(max_release)
+
+
+def part2(valves):
+    # from pprint import pprint
+
+    tunnel_costs = determine_tunnel_costs(valves)
+    max_releases = [
+        res for res in _valve(valves, tunnel_costs, "AA", (), 0, 26) if res[0]
+    ]
+
+    # pprint(max_releases)
+
+    disjoint_releases = []
+    for res1, res2 in product(max_releases, max_releases):
+        if (res1[1] - {"AA"}).isdisjoint(res2[1]):
+            disjoint_releases.append(res1[0] + res2[0])
+
+    return max(disjoint_releases)
 
 
 if __name__ == "__main__":
@@ -84,5 +105,9 @@ if __name__ == "__main__":
     # pprint(valves)
     # pprint(determine_tunnel_costs(valves))
     # print()
-    # print("part1 test", part1(read("16-test.txt"), "AA", 30))
-    print("part1", part1(read("16.txt"), "AA", 30))
+
+    # print("part1 test", part1(read("16-test.txt")))
+    # print("part2 test", part2(read("16-test.txt")))
+
+    print("part1", part1(read("16.txt")))
+    print("part2", part2(read("16.txt")))
