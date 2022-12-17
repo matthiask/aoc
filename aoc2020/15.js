776d8bdf186616fbbcffd818b8a3f2d86f9c4615
Matthias Kestenholz <mk@feinheit.ch>
2022-12-17 20:33:26 +0100

    2020 15

--- aoc2020/15.js
+++ aoc2020/15.js
@@ -1,6 +1,7 @@
 const test = [0, 3, 6]
 const input = [6, 4, 12, 1, 20, 0, 16]
 
+/*
 const iterate = (sequence, rounds) => {
   const numbers = [...sequence]
 
@@ -20,4 +21,34 @@ const iterate = (sequence, rounds) => {
 console.log("part1 test", test, iterate(test, 10))
 console.log("part1 test", test, iterate(test, 2020))
 console.log("part1", input, iterate(input, 2020))
-console.log("part2", input, iterate(input, 30_000_000))
+// console.log("part2", input, iterate(input, 30_000_000))
+*/
+
+const solve = (log, sequence, rounds) => {
+  const numbers = new Map()
+  for (let i = 0; i < sequence.length - 1; ++i) {
+    numbers.set(sequence[i], 1 + i)
+  }
+  let lastNumber = sequence.at(-1)
+  let number
+
+  for (let i = sequence.length; i < rounds; ++i) {
+    if (numbers.has(lastNumber)) {
+      const round = numbers.get(lastNumber)
+      number = i - round
+    } else {
+      number = 0
+    }
+
+    // console.log({ i, number })
+
+    numbers.set(lastNumber, i)
+    lastNumber = number
+    // lastRound = round
+  }
+  console.log(log, { lastNumber, number })
+}
+
+solve("test", test, 2020)
+solve("part1", input, 2020)
+solve("part2", input, 30_000_000)
