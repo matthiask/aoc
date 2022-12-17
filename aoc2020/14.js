f8b02a0e9fabc5cda4bd0591e8cbf6b8cf4a2c0f
Matthias Kestenholz <mk@feinheit.ch>
2022-12-17 19:16:35 +0100

    2020 14

--- aoc2020/14.js
+++ aoc2020/14.js
@@ -8,14 +8,21 @@ mem[7] = 101
 mem[8] = 0
 `
 
-const dec2bin = (number) => (number >>> 0).toString(2)
+const test2 = `\
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+`
+
+const dec2bin = (number) => (Number(number) >>> 0).toString(2).padStart(36, "0")
 const bin2dec = (number) => parseInt(number, 2)
 
 const applyMask = (mask, number) => {
   const bin = Array.from(mask)
     .map((m, idx) => (m === "X" ? number[idx] : m))
     .join("")
-  console.debug({ mask, number, bin })
+  // console.debug({ mask, number, bin })
   return bin2dec(bin)
 }
 
@@ -32,7 +39,7 @@ const parse = (input) => {
         program.push([
           "mem",
           Number(left.replace("mem[", "").replace("]", "")),
-          dec2bin(Number(right)).padStart(36, "0"),
+          dec2bin(right),
         ])
       } else {
         throw Error()
@@ -59,6 +66,57 @@ const part1 = (input) => {
   return Array.from(memory.values()).reduce((a, b) => a + b, 0)
 }
 
+const allAddresses = (mask, number) => {
+  number = dec2bin(number)
+  const floating = []
+  let num = Array.from(mask).map((m, idx) => {
+    if (m === "X") {
+      floating.push(idx)
+    } else if (m === "0") {
+      return number[idx]
+    }
+    return m
+  })
+
+  const floatingNumbers = 1 << floating.length
+  const addresses = []
+  for (let n = 0; n < floatingNumbers; ++n) {
+    const bits = n.toString(2).padStart(floating.length, "0")
+    // console.debug(bits)
+    floating.forEach((index, idx) => {
+      num[index] = bits[idx]
+    })
+    // console.debug(num)
+    addresses.push(bin2dec(num.join("")))
+  }
+
+  // console.debug({ mask, number, floating, addresses })
+  return addresses
+}
+
+const part2 = (input) => {
+  const program = parse(input)
+  const memory = new Map()
+
+  let mask = ""
+
+  for (let [inst, op1, op2] of program) {
+    if (inst == "msk") {
+      mask = op2
+    } else {
+      for (let address of allAddresses(mask, op1)) {
+        memory.set(address, bin2dec(op2))
+      }
+    }
+  }
+
+  // console.debug(memory)
+  return Array.from(memory.values()).reduce((a, b) => a + b, 0)
+}
+
 console.debug(parse(test))
 console.debug("part1 test", part1(test))
 console.debug("part1", part1(input))
+
+console.debug("part2 test", part2(test2))
+console.debug("part2", part2(input))
