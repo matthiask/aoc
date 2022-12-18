5f61d645e6ca13dcf2c8871e19203f7ec36d13d4
Matthias Kestenholz <mk@feinheit.ch>
2022-12-18 12:31:00 +0100

    2022 18 but runs dog slow because it rediscovers the air around potential bubbles from the start every time

--- aoc2022/18.py
+++ aoc2022/18.py
@@ -1,3 +1,6 @@
+from itertools import chain, product
+
+
 with open("18.txt") as f:
     puzzle_input = f.read()
 test_input = """\
@@ -32,7 +35,7 @@ _adjacent = [
 
 
 def adjacent_coordinates(point):
-    return [tuple(a + b for a, b in zip(point, offset)) for offset in _adjacent]
+    return {tuple(a + b for a, b in zip(point, offset)) for offset in _adjacent}
 
 
 def surface_area(cubes):
@@ -43,6 +46,71 @@ def surface_area(cubes):
     return surface
 
 
+def _expand(cubes, edge_cubes, bubble):
+    # print("Checking", bubble)
+    seen = set()
+    while True:
+        expanded = False
+        unchecked_adjacent = (
+            set(chain.from_iterable(adjacent_coordinates(cube) for cube in bubble))
+            - seen
+        )
+
+        for potential_cube in unchecked_adjacent:
+            if potential_cube in cubes:
+                # print("Adjacent cube is solid", potential_cube)
+                continue
+            if potential_cube in edge_cubes:
+                # print("Encountered edge cube", potential_cube)
+                return None
+
+            expanded = True
+            bubble.add(potential_cube)
+
+        seen |= unchecked_adjacent
+
+        if not expanded:
+            # print("No additional expansion, returning", bubble)
+            return bubble
+        # print("Expanding", bubble)
+
+
+def find_bubbles(cubes):
+    xmin = min(point[0] for point in cubes)
+    ymin = min(point[1] for point in cubes)
+    zmin = min(point[2] for point in cubes)
+    xmax = max(point[0] for point in cubes)
+    ymax = max(point[1] for point in cubes)
+    zmax = max(point[2] for point in cubes)
+
+    all_cubes = set(
+        product(
+            range(xmin, xmax + 1),
+            range(ymin, ymax + 1),
+            range(zmin, zmax + 1),
+        )
+    )
+    all_inner_cubes = set(
+        product(
+            range(xmin + 1, xmax),
+            range(ymin + 1, ymax),
+            range(zmin + 1, zmax),
+        )
+    )
+    edge_cubes = all_cubes - all_inner_cubes
+
+    # print("edge_cubes", sorted(edge_cubes))
+    print("Set sizes", len(all_cubes), len(all_inner_cubes), len(edge_cubes))
+
+    bubbles = set()
+    for possible_bubble_start in sorted(all_inner_cubes - cubes):
+        # print("Started at", possible_bubble_start, "found", _expand(cubes, edge_cubes, {possible_bubble_start}))
+
+        if bubble := _expand(cubes, edge_cubes, {possible_bubble_start}):
+            bubbles.add(tuple(sorted(bubble)))
+    return bubbles
+
+
 if __name__ == "__main__":
     from pprint import pprint
 
@@ -50,4 +118,17 @@ if __name__ == "__main__":
     pprint(adjacent_coordinates((2, 2, 2)))
 
     print("part1 test", surface_area(parse(test_input)))
-    print("part1", surface_area(parse(puzzle_input)))
+    cubes = parse(puzzle_input)
+    print("part1", surface_area(cubes))
+
+    print("\npart2 test")
+    cubes = parse(test_input)
+    bubbles = find_bubbles(cubes)
+    print([(len(bubble), surface_area(bubble)) for bubble in bubbles])
+    print(surface_area(cubes) - sum(surface_area(bubble) for bubble in bubbles))
+
+    print("\npart2")
+    cubes = parse(puzzle_input)
+    bubbles = find_bubbles(cubes)
+    print([(len(bubble), surface_area(bubble)) for bubble in bubbles])
+    print(surface_area(cubes) - sum(surface_area(bubble) for bubble in bubbles))
