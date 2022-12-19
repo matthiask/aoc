39d07cf22adf4f195dee41c0828f1e2457294c97
Matthias Kestenholz <mk@feinheit.ch>
2022-12-19 18:13:20 +0100

    Thinking about recursion

--- aoc2020/17.js
+++ aoc2020/17.js
@@ -31,9 +31,22 @@ const parse = (input, zero) => {
   return points
 }
 
+/*
 // https://stackoverflow.com/a/43053803
 const cartesian = (...a) =>
   a.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())))
+*/
+
+const cartesian = (head, ...tail) => {
+  const next = tail.length ? cartesian(...tail) : [[]]
+  const results = []
+  for (let n of next) {
+    for (let h of head) {
+      results.push([h, ...n])
+    }
+  }
+  return results
+}
 
 const neighbors = (point) => {
   const neighbors = []
