0ce835c897611825e5efdfe18bca53490f9339f8
Matthias Kestenholz <mk@feinheit.ch>
2022-12-19 14:26:07 +0100

    2020 16 part1

--- aoc2020/16.js
+++ aoc2020/16.js
@@ -15,3 +15,47 @@ nearby tickets:
 55,2,20
 38,6,12
 `
+
+const parse = (input) => {
+  const [ruleLines, myTicket, nearbyTickets] = input.split("\n\n")
+
+  const rules = {}
+  ruleLines.split("\n").forEach((line) => {
+    const [name, rangePieces] = line.split(": ")
+    const ranges = rangePieces
+      .split(" or ")
+      .map((range) =>
+        range.split("-").map((number, idx) => Number(number) + idx),
+      )
+    rules[name] = {
+      ranges,
+      test(number) {
+        return ranges.some((range) => range[0] <= number && number < range[1])
+      },
+    }
+  })
+
+  return {
+    rules,
+    myTicket: myTicket.split("\n")[1].split(",").map(Number),
+    nearbyTickets: nearbyTickets
+      .split("\n")
+      .filter(Boolean)
+      .slice(1)
+      .map((line) => line.split(",").map(Number)),
+  }
+}
+
+const part1 = (input) => {
+  const { rules, nearbyTickets } = parse(input)
+  const allNumbers = nearbyTickets.flat()
+  const invalidNumbers = allNumbers.filter(
+    (number) => !Object.values(rules).some((rule) => rule.test(number)),
+  )
+  // console.log(allNumbers, invalidNumbers)
+  return invalidNumbers.reduce((a, b) => a + b, 0)
+}
+
+console.debug(parse(test))
+console.log("part1 test", part1(test))
+console.log("part1", part1(input))
