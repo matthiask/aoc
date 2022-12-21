dbbce0bda9f3a51c563cd019647ef3fd94771bdd
Matthias Kestenholz <mk@feinheit.ch>
2022-12-21 09:23:51 +0100

    toposort not required

--- aoc2022/21.py
+++ aoc2022/21.py
@@ -1,6 +1,5 @@
 import operator
 from dataclasses import dataclass
-from functools import reduce
 
 
 input = open("21.txt").read()
@@ -25,51 +24,6 @@ hmdt: 32
 """
 
 
-# django/utils/topological_sort.py
-class CyclicDependencyError(ValueError):
-    pass
-
-
-def topological_sort_as_sets(dependency_graph):
-    """
-    Variation of Kahn's algorithm (1962) that returns sets.
-
-    Take a dependency graph as a dictionary of node => dependencies.
-
-    Yield sets of items in topological order, where the first set contains
-    all nodes without dependencies, and each following set contains all
-    nodes that may depend on the nodes only in the previously yielded sets.
-    """
-    todo = dependency_graph.copy()
-    while todo:
-        current = {node for node, deps in todo.items() if not deps}
-
-        if not current:
-            raise CyclicDependencyError(
-                "Cyclic dependency in graph: {}".format(
-                    ", ".join(repr(x) for x in todo.items())
-                )
-            )
-
-        yield current
-
-        # remove current from todo's nodes & dependencies
-        todo = {
-            node: (dependencies - current)
-            for node, dependencies in todo.items()
-            if node not in current
-        }
-
-
-def stable_topological_sort(nodes, dependency_graph):
-    result = []
-    for layer in topological_sort_as_sets(dependency_graph):
-        for node in nodes:
-            if node in layer:
-                result.append(node)
-    return result
-
-
 _operations = {
     "+": operator.add,
     "-": operator.sub,
@@ -88,9 +42,8 @@ class Calculation:
     def value(self, calculations):
         if self._value is not None:
             return self._value
-        return reduce(
-            _operations[self.operation],
-            (calculations[name].value(calculations) for name in self.dependencies),
+        return _operations[self.operation](
+            *[calculations[name].value(calculations) for name in self.dependencies]
         )
 
 
@@ -109,17 +62,6 @@ def parse_calculation(line):
 
 def parse(input):
     return dict(parse_calculation(line) for line in input.strip().splitlines())
-
-
-def evaluate(calculations):
-    evaluation_ordering = stable_topological_sort(
-        calculations.keys(),
-        {key: calculation.dependency_list for key, calculation in calculations.items()},
-    )
-
-    values = {}
-    for name in evaluation_ordering:
-        values[name] = 0
 
 
 if __name__ == "__main__":
