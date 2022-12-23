68cfcd90f5031c72920dfb0fe067e92f72ec221f
Matthias Kestenholz <mk@feinheit.ch>
2022-12-23 09:30:08 +0100

    Nice

--- aoc2018/day02/src/main.rs
+++ aoc2018/day02/src/main.rs
@@ -1,7 +1,9 @@
+use levenshtein::levenshtein;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
 use std::io::{self, BufRead};
+use std::iter::zip;
 
 type Frequencies = HashMap<i32, i32>;
 
@@ -25,14 +27,46 @@ fn count_frequencies(id: &str, frequencies: &mut Frequencies) {
     }
 }
 
+fn find_closest_strings(lines: &Vec<String>) -> (usize, usize) {
+    for i in 0..lines.len() {
+        for j in i..lines.len() {
+            let a = &lines[i];
+            let b = &lines[j];
+            let distance = levenshtein(&a, &b);
+            if distance == 1 {
+                return (i, j);
+                // println!("{} .. {}: {}", a, b, distance);
+            }
+        }
+    }
+    (0, 0)
+}
+
+fn remove_mismatch(a: &String, b: &String) -> String {
+    let zipper = zip(a.chars(), b.chars());
+    let mut chars: Vec<char> = Vec::new();
+
+    for (c1, c2) in zipper {
+        if c1 == c2 {
+            chars.push(c1);
+        }
+    }
+
+    chars.iter().collect()
+    // "Nothing".to_string()
+}
+
 fn main() -> io::Result<()> {
     let file = fs::File::open("02.txt")?;
     let reader = io::BufReader::new(file);
 
     let mut frequencies: Frequencies = HashMap::new();
+    let mut lines: Vec<String> = Vec::new();
 
     for line in reader.lines() {
-        count_frequencies(&line.unwrap(), &mut frequencies);
+        let v = line.unwrap();
+        count_frequencies(&v, &mut frequencies);
+        lines.push(v);
     }
 
     let times_2 = frequencies.get(&2).unwrap_or(&0);
@@ -40,6 +74,14 @@ fn main() -> io::Result<()> {
 
     println!("2: {}, 3: {}", &times_2, &times_3);
     println!("checksum: {}", times_2 * times_3);
+
+    let (i, j) = find_closest_strings(&lines);
+    println!("{}, {}", i, j);
+
+    println!(
+        "without mismatch: {}",
+        remove_mismatch(&lines[i], &lines[j])
+    );
 
     Ok(())
 }
