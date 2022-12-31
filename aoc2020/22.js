92f4bba80dcba8aedb54e03c36352f472afa92e6
Matthias Kestenholz <mk@feinheit.ch>
2022-12-31 17:35:17 +0100

    2020 22

--- aoc2020/22.js
+++ aoc2020/22.js
@@ -38,11 +38,56 @@ const round = (deck1, deck2) => {
   }
 }
 
-const winningDeck = (deck1, deck2) => {
+const untilWin = (deck1, deck2) => {
   while (deck1.length && deck2.length) {
     round(deck1, deck2)
   }
+}
+
+const winningDeck = (deck1, deck2) => {
+  untilWin(deck1, deck2)
   return deck1.length ? deck1 : deck2
+}
+
+// PART 2 ########################################
+
+const round2 = (deck1, deck2) => {
+  const top1 = deck1.shift()
+  const top2 = deck2.shift()
+
+  let winner
+
+  if (top1 <= deck1.length && top2 <= deck2.length) {
+    winner = playGame(deck1.slice(0, top1), deck2.slice(0, top2))
+  } else {
+    winner = top1 > top2 ? 1 : 2
+  }
+
+  if (winner === 1) {
+    deck1.push(top1, top2)
+  } else if (winner === 2) {
+    deck2.push(top2, top1)
+  } else {
+    console.debug("Unexpected winner value", winner)
+    throw Error()
+  }
+}
+
+const playGame = (deck1, deck2) => {
+  // console.debug("Playing game with", deck1, deck2)
+  const memo = new Set()
+
+  while (deck1.length && deck2.length) {
+    const key = `${deck1.join(",")}|${deck2.join(",")}`
+    if (memo.has(key)) {
+      return 1
+    }
+    memo.add(key)
+
+    round2(deck1, deck2)
+  }
+
+  return deck1.length ? 1 : 2
 }
 
 const score = (deck) => {
@@ -52,10 +97,27 @@ const score = (deck) => {
 }
 
 const solve = (input) => {
-  const decks = parse(input)
-
+  let decks = parse(input)
   console.log("part1", score(winningDeck(...decks)))
+
+  decks = parse(input)
+  playGame(...decks)
+  // console.debug(decks)
+  console.log("part2", score(decks[0].length ? decks[0] : decks[1]))
 }
 
 solve(test)
 solve(input)
+
+/*
+solve(`\
+Player 1:
+43
+19
+
+Player 2:
+2
+29
+14
+`)
+*/
