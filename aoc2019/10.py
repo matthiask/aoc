8588a0415d7453f9f0d532572022144df6670b71
Matthias Kestenholz <mk@feinheit.ch>
2022-12-31 23:16:34 +0100

    That was nice

--- aoc2019/10.py
+++ aoc2019/10.py
@@ -1,5 +1,7 @@
+import math
 import sys
-from math import gcd
+from collections import defaultdict
+from itertools import cycle
 
 
 grid = [*open(sys.argv[1] if len(sys.argv) > 1 else "10.txt")]
@@ -11,26 +13,31 @@ points = [
 ]
 
 
-# def sign(v):
-#     if v > 0:
-#         return 1
-#     elif v < 0:
-#         return -1
-#     return 0
-
-
 def visibility_spec(from_point, to_point):
+    """
+    atan2 goes from -pi to +pi
+
+    atan2(+0.00001, -1) ~= +pi
+    atan2(-0.00001, -1) ~= -pi
+    atan2(0, -1) == pi
+    atan2(0, 1) == pi
+
+    >>> visibility_spec((0, 0), (0, -1))
+    0.0
+    >>> 1.5 < visibility_spec((0, 0), (1, 0)) < 1.6
+    True
+    >>> 3 <= visibility_spec((0, 0), (0, 1)) <= 3.2
+    True
+    >>> 4.7 < visibility_spec((0, 0), (-1, 0)) < 4.8
+    True
+    >>> 6.2 < visibility_spec((0, 0), (-0.0001, -1))
+    True
+    """
     vec = [
         to_point[0] - from_point[0],
         to_point[1] - from_point[1],
     ]
-    d = gcd(*vec)
-    return (
-        # sign(vec[0]),
-        # sign(vec[1]),
-        vec[0] // d,
-        vec[1] // d,
-    )
+    return math.pi - math.atan2(*vec)
 
 
 def visible_asteroids_from(p):
@@ -38,4 +45,26 @@ def visible_asteroids_from(p):
 
 
 # print(grid)
-print("part1", max(visible_asteroids_from(point) for point in points))
+visibility_counts = sorted((visible_asteroids_from(point), point) for point in points)
+print("part1", visibility_counts[-1][0])
+
+station = visibility_counts[-1][1]
+directions = defaultdict(list)
+for point in points:
+    spec = visibility_spec(station, point)
+    directions[spec].append(point)
+asteroids = cycle(
+    [
+        sorted(points, key=lambda point: math.dist(station, point))
+        for angle, points in sorted(directions.items())
+    ]
+)
+vaporized = 0
+for direction in asteroids:
+    if direction:
+        vaporized += 1
+        if vaporized == 200:
+            print(direction.pop(0))
+            break
+        else:
+            direction.pop()
