bc9c754848c7c5317d0de793688ffc287a3b457e
Matthias Kestenholz <mk@feinheit.ch>
2023-01-03 23:42:43 +0100

    Nice exercise

---
+++ aoc2015/07.py
@@ -0,0 +1,29 @@
+IN = [*open("07.txt")]
+wires = {}
+
+
+def value(v):
+    if v in wires:
+        return wires[v]()
+    return int(v)
+
+
+for line in IN:
+    op, tg = line.strip().split(" -> ")
+
+    match op.split():
+        case (w1, "AND", w2):
+            wires[tg] = lambda w1=w1, w2=w2: value(w1) & value(w2)
+        case (w1, "OR", w2):
+            wires[tg] = lambda w1=w1, w2=w2: value(w1) | value(w2)
+        case (w1, "LSHIFT", w2):
+            wires[tg] = lambda w1=w1, w2=w2: value(w1) << value(w2)
+        case (w1, "RSHIFT", w2):
+            wires[tg] = lambda w1=w1, w2=w2: value(w1) >> value(w2)
+        case ("NOT", w1):
+            wires[tg] = lambda w1=w1: ~value(w1)
+        case (w1,):
+            wires[tg] = lambda w1=w1: value(w1)
+
+
+print(wires["a"]())
