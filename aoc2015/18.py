6199166700e347ceb95300a315fe8763d7307db6
Matthias Kestenholz <mk@feinheit.ch>
2023-01-04 23:28:50 +0100

    One less global

--- aoc2015/18.py
+++ aoc2015/18.py
@@ -1,15 +1,10 @@
-# part1
-always_on = set()
-# part2
-always_on = {0, 99, 99j, 99 + 99j}
-
 IN = [*open("18.txt")]
 on = {
     complex(x, y)
     for y, line in enumerate(IN)
     for x, c in enumerate(line.strip())
     if c == "#"
-} | always_on
+}
 dim = 100
 
 
@@ -36,11 +31,17 @@ def next(on):
         p
         for p, is_on, surrounding_on in all_
         if (is_on and 2 <= surrounding_on <= 3) or (not is_on and surrounding_on == 3)
-    } | always_on
+    }
 
 
 if __name__ == "__main__":
+    p1 = set(on)
     for _ in range(100):
-        # print(len(on))
-        on = next(on)
-    print(len(on))
+        p1 = next(p1)
+    print(len(p1))
+
+    always_on = {0, 99, 99j, 99 + 99j}
+    p2 = on | always_on
+    for _ in range(100):
+        p2 = next(p2) | always_on
+    print(len(p2))
