8267074d8628cf074c2bf536ca756cc841d2bdee
Matthias Kestenholz <mk@feinheit.ch>
2023-01-04 23:02:30 +0100

    Nice, finally a time to exercise some itertools utilities I haven't used before IIRC

---
+++ aoc2015/15.py
@@ -0,0 +1,36 @@
+import operator
+import re
+from collections import defaultdict
+from functools import reduce
+from itertools import combinations_with_replacement
+
+
+IN = [*open("15.txt")]
+ingredients = {}
+for line in IN:
+    name, properties = line.split(": ")
+    ingredients[name] = [
+        (prop, int(val)) for prop, val in re.findall(r"(\w+) ([-\d]+)", properties)
+    ]
+
+
+def score(combination):
+    values = defaultdict(int)
+    for name, properties in ingredients.items():
+        for prop, value in properties:
+            values[prop] += combination.count(name) * value
+
+    # Part 2
+    if values["calories"] != 500:
+        return 0
+
+    return reduce(
+        operator.mul,
+        (max(0, value) for prop, value in values.items() if not prop == "calories"),
+        1,
+    )
+
+
+# print(ingredients)
+combinations = combinations_with_replacement(ingredients.keys(), 100)
+print(max(map(score, combinations)))
