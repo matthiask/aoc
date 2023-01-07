c7551b10fb4e47a375f2370f5cc9c9e1eea76c18
Matthias Kestenholz <mk@feinheit.ch>
2023-01-07 21:36:33 +0100

    Today I also learned about lookaheads

--- aoc2016/07.py
+++ aoc2016/07.py
@@ -5,26 +5,39 @@ from dataclasses import dataclass
 @dataclass
 class Record:
     line: str
-    outside: list[str]
-    inside: list[str]
+    supernet: list[str]
+    hypernet: list[str]
 
 
 def parse_line(line):
     parts = re.split(r"(\[\w+\])", line)
-    outside = [part for part in parts if part[0] != "["]
-    inside = [part[1:-1] for part in parts if part[0] == "["]
-    return Record(line, outside, inside)
+    supernet = [part for part in parts if part[0] != "["]
+    hypernet = [part[1:-1] for part in parts if part[0] == "["]
+    return Record(line, supernet, hypernet)
 
 
 def supports_tls(record):
-    return any(abba_re.search(p) for p in record.outside) and all(
-        not abba_re.search(p) for p in record.inside
+    return any(abba_re.search(p) for p in record.supernet) and all(
+        not abba_re.search(p) for p in record.hypernet
     )
+
+
+def supports_ssl(record):
+    for p in record.supernet:
+        for aba in aba_re.findall(p):
+            # print(aba)
+            bab = aba[1] + aba[0] + aba[1]
+            if any(bab in p2 for p2 in record.hypernet):
+                print(aba, bab, record.line)
+                return True
+    return False
 
 
 IN = [parse_line(line.strip()) for line in open("07.txt")]
 abba_re = re.compile(r"(\w)((?!\1)\w)\2\1")
+aba_re = re.compile(r"(?=(\w)((?!\1)\w)\1)")
 
 
 # print(IN)
 print(sum(1 for record in IN if supports_tls(record)))
+print(sum(1 for record in IN if supports_ssl(record)))
