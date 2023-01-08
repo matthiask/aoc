65cf49de91c3795c57b1c10fee5106d998e17b72
Matthias Kestenholz <mk@feinheit.ch>
2023-01-08 17:45:52 +0100

    That was interesting

---
+++ aoc2016/18.py
@@ -0,0 +1,36 @@
+IN = "......^.^^.....^^^^^^^^^...^.^..^^.^^^..^.^..^.^^^.^^^^..^^.^.^.....^^^^^..^..^^^..^^.^.^..^^..^^^.."
+
+
+def next_row(row):
+    def _grouped():
+        s = "." + row + "."
+        for i in range(1, len(s) - 1):
+            # print(i, s[i - 1 : i + 2])
+            yield s[i - 1 : i + 2]
+
+    return "".join(
+        "^"
+        if group
+        in {
+            "^^.",
+            ".^^",
+            "^..",
+            "..^",
+        }
+        else "."
+        for group in _grouped()
+    )
+
+
+def expand(first, lines):
+    grid = [first]
+    while len(grid) < lines:
+        grid.append(next_row(grid[-1]))
+    return grid
+
+
+# print(sum(line.count(".") for line in grid))
+
+print("\n".join(expand("..^^.", 3)))
+print(sum(line.count(".") for line in expand(IN, 40)))
+print(sum(line.count(".") for line in expand(IN, 400000)))
