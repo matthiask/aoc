e84a8db0c84e82b33ec3500c1602692d559ef9b7
Matthias Kestenholz <mk@feinheit.ch>
2023-01-08 19:51:00 +0100

    A nice problem, again

--- aoc2016/20.py
+++ aoc2016/20.py
@@ -1,12 +1,44 @@
+# The input uses [] intervals, we use [) for convenience
 IN = [*open("20.txt")]
-blocklist = [tuple(int(n) for n in line.strip().split("-")) for line in IN]
+blocklist = [
+    tuple(int(n) + i for i, n in enumerate(line.strip().split("-"))) for line in IN
+]
 
 
 def part1():
-    candidates = sorted(r[1] + 1 for r in blocklist)
+    candidates = sorted(r[1] for r in blocklist)
     for candidate in candidates:
-        if not any(r[0] <= candidate <= r[1] for r in blocklist):
+        if not any(r[0] <= candidate < r[1] for r in blocklist):
             return candidate
 
 
+def overlaps(range1, range2):
+    s1, e1 = range1
+    s2, e2 = range2
+    return e1 >= s2 and e2 >= s1
+
+
+def union(range1, range2):
+    s1, e1 = range1
+    s2, e2 = range2
+    return (min(s1, s2), max(e1, e2))
+
+
+def part2():
+    ranges = []
+    for block in blocklist:
+        with_overlaps = block
+        no_overlaps = []
+        for r in ranges:
+            if overlaps(r, with_overlaps):
+                with_overlaps = union(with_overlaps, r)
+            else:
+                no_overlaps.append(r)
+        ranges = sorted(no_overlaps + [with_overlaps])
+
+    # print("ranges", ranges)
+    return 2**32 - sum(r[1] - r[0] for r in ranges)
+
+
 print(part1())
+print(part2())
