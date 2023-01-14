c8c7c5129c8b23e9966aef569eeca484d42fcb38
Matthias Kestenholz <mk@feinheit.ch>
2023-01-14 13:28:31 +0100

    More unused stuff

--- aoc2016/22.py
+++ aoc2016/22.py
@@ -1,4 +1,5 @@
 import re
+import sys
 from dataclasses import dataclass
 from itertools import product
 from pprint import pprint
@@ -16,9 +17,18 @@ class Node:
 
 IN = [
     Node(*[int(n) for n in re.findall(r"\d+", line)])
-    for line in list(open("22.txt"))[2:]
+    for line in list(open("22.txt" if len(sys.argv) < 2 else sys.argv[1]))[2:]
 ]
 grid = {(node.x, node.y): node for node in IN}
+
+
+def part1():
+    def is_viable(node1, node2):
+        if node1 == node2:
+            return False
+        return node1.used and node1.used < node2.avail
+
+    return sum(1 for pair in product(IN, IN) if is_viable(*pair))
 
 
 def neighbors(node):
@@ -31,18 +41,11 @@ def neighbors(node):
     return [node for node in map(grid.get, coords) if node]
 
 
-def part1():
-    def is_viable(node1, node2):
-        if node1 == node2:
-            return False
-        return node1.used and node1.used < node2.avail
-
-    return sum(1 for pair in product(IN, IN) if is_viable(*pair))
-
-
 def part2():
-    access = (0, 32)
+    data = (0, 32)
+    access = (0, 0)
+    empty_node = 0  # ??
 
 
-pprint(IN)
+pprint(grid)
 pprint(("part1", part1()))
