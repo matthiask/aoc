e8b684e6e63833094c863adcb90449131f33b38b
Matthias Kestenholz <mk@feinheit.ch>
2023-01-21 13:46:32 +0100

    Nice

---
+++ aoc2017/05.mjs
@@ -0,0 +1,40 @@
+import { readFileSync } from "fs"
+const input = readFileSync("05.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter(Boolean)
+  .map(Number)
+
+// console.debug(input)
+
+const part1 = () => {
+  let program = [...input]
+  let ip = 0
+  for (let i = 1; ; ++i) {
+    ip += program[ip]++
+
+    if (0 <= ip && ip < program.length) continue
+    console.log("part1", i)
+    break
+  }
+}
+
+const part2 = () => {
+  let program = [...input]
+  let ip = 0
+  for (let i = 1; ; ++i) {
+    let o = program[ip]
+    if (o >= 3) {
+      --program[ip]
+    } else {
+      ++program[ip]
+    }
+    ip += o
+
+    if (0 <= ip && ip < program.length) continue
+    console.log("part2", i)
+    break
+  }
+}
+
+part1()
+part2()
