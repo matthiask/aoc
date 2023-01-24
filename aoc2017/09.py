b230ddcd6c3d506a1b2013ab57297fba4dc26ae2
Matthias Kestenholz <mk@feinheit.ch>
2023-01-24 18:13:55 +0100

    Soo that doesn't work yet

---
+++ aoc2017/09.py
@@ -0,0 +1,54 @@
+from utils import open_input
+
+
+def score(chars):
+    """
+    >>> score("{}")
+    1
+    >>> score("{{{}}}")
+    3
+    >>> score("{{},{}}")
+    3
+    >>> score("{{{},{},{{}}}}")
+    6
+    >>> score("{<{},{},{{}}>}")
+    1
+    >>> score("{<a>,<a>,<a>,<a>}")
+    1
+    >>> score("{{<a>},{<a>},{<a>},{<a>}}")
+    5
+    >>> score("{{<!>},{<!>},{<!>},{<a>}}")
+    2
+    """
+
+    score = 0
+    idx = 0
+    length = len(chars)
+
+    while idx < length:
+        if chars[idx] == "{":
+            idx += 1
+        elif chars[idx] == "}":
+            score += 1
+            idx += 1
+        elif chars[idx] == "!":
+            # Skip next
+            idx += 2
+        elif chars[idx] == "<":
+            idx += 1
+
+            while True:
+                if chars[idx] == "!":
+                    idx += 2
+                elif chars[idx] == ">":
+                    idx += 1
+                    break
+                else:
+                    idx += 1
+        else:
+            idx += 1
+
+    return score
+
+
+print("part1", score(open_input("09").read().strip()))
