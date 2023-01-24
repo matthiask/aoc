96392ba3e9b9b50c472c2fe07e2d2943b3b79a45
Matthias Kestenholz <mk@feinheit.ch>
2023-01-24 09:45:59 +0100

    2017 07

--- aoc2017/07.py
+++ aoc2017/07.py
@@ -1,6 +1,7 @@
 import operator
 import re
-from functools import reduce
+from collections import Counter
+from functools import cache, reduce
 
 from utils import open_input
 
@@ -8,17 +9,37 @@ from utils import open_input
 IN = [*open_input("07")]
 
 nodes = {}
+parent = {}
 for line in IN:
     tokens = re.sub(r"[->(),]+", "", line).split()
-    nodes[tokens[0]] = [int(tokens[1]), set(tokens[2:])]
+    nodes[tokens[0]] = (w, children) = [int(tokens[1]), set(tokens[2:])]
+    for child in children:
+        parent[child] = tokens[0]
 (root,) = tuple(set(nodes) - reduce(operator.or_, (pair[1] for pair in nodes.values())))
 
 
+@cache
 def weight(node):
     w, children = nodes[node]
     return w + sum(weight(child) for child in children)
 
 
+def unbalanced_node_weight(node):
+    w, children = nodes[node]
+    if not children:
+        # Leaf nodes cannot be unbalanced
+        return
+    if len({weight(child) for child in children}) != 1:
+        # Children are unbalanced
+        return
+    siblings = Counter(weight(child) for child in nodes[parent[node]][1]).most_common()
+    if len(siblings) != 2:
+        return
+    if weight(node) == siblings[0][0]:
+        return
+    return siblings[0][0] - sum(weight(child) for child in children)
+
+
 # print(nodes)
 print("part1", root)
-print("part2", [(child, weight(child)) for child in nodes[root][1]])
+print("part2", next(filter(None, (unbalanced_node_weight(node) for node in nodes))))
