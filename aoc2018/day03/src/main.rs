f0d5916f33076e39a8572d4dbdbf8cd62dc7d15a
Matthias Kestenholz <mk@feinheit.ch>
2023-02-03 21:31:22 +0100

    Yay

--- aoc2018/day03/src/main.rs
+++ aoc2018/day03/src/main.rs
@@ -5,11 +5,11 @@ use std::collections::HashMap;
 
 #[derive(Copy, Clone)]
 struct Rect {
+    id: i64,
     x: i64,
     y: i64,
     w: i64,
     h: i64,
-    negative: bool,
 }
 
 impl Rect {
@@ -19,31 +19,6 @@ impl Rect {
 
     fn y_outside(&self) -> i64 {
         self.y + self.h
-    }
-
-    fn size(&self) -> i64 {
-        let area = self.w * self.h ;
-        if self.negative {
-            return -area;
-        }
-        area
-    }
-
-    fn intersect(&self, other: &Rect) -> Option<Rect> {
-        if !self.overlaps(&other) {
-            return None;
-        }
-        let x = std::cmp::max(self.x, other.x);
-        let y = std::cmp::max(self.y, other.y);
-        let w = std::cmp::min(self.x_outside(), other.x_outside()) - x;
-        let h = std::cmp::min(self.y_outside(), other.y_outside()) - y;
-        Some(Rect {
-            x,
-            y,
-            w,
-            h,
-            negative: true,
-        })
     }
 
     fn overlaps(&self, other: &Rect) -> bool {
@@ -60,11 +35,11 @@ fn parse_claim(line: &str) -> Rect {
         .collect();
     // println!("parts: {:?}", &parts);
     Rect {
+        id: parts[1].parse::<i64>().unwrap(),
         x: parts[4].parse::<i64>().unwrap(),
         y: parts[5].parse::<i64>().unwrap(),
         w: parts[7].parse::<i64>().unwrap(),
         h: parts[8].parse::<i64>().unwrap(),
-        negative: false,
     }
 }
 
@@ -81,32 +56,11 @@ fn main() -> io::Result<()> {
 
     println!("claims: {}", claims.len());
 
-    /*
-    let mut rects = Vec::new();
-    for rect in claims {
-        for i in 0..rects.len() {
-            if let Some(x) = rect.intersect(&rects[i]) {
-                rects.push(rect);
-                rects.push(rects[i]);
-                rects.push(x);
-            }
-        }
-        rects.push(rect);
-    }
-
-    let mut area = 0;
-    for rect in rects {
-        area += rect.size();
-    }
-
-    println!("area: {}", area);
-    */
-
     let mut occupations: HashMap<i64, i64> = HashMap::new();
-    for claim in claims {
-        for dx in 0..claim.w {
-            for dy in 0..claim.h {
-                let v = (claim.y + dy) * 10000 + (claim.x + dx);
+    for claim in &claims {
+        for x in claim.x..claim.x_outside() {
+            for y in claim.y..claim.y_outside() {
+                let v = y * 10000 + x;
                 let n = occupations.get(&v).unwrap_or(&0);
                 occupations.insert(v, n + 1);
             }
@@ -120,6 +74,21 @@ fn main() -> io::Result<()> {
     }
 
     println!("part1: {}", more_than_one);
+
+    for i in 0..claims.len() {
+        let mut overlaps = false;
+        for j in 0..claims.len() {
+            if i != j && claims[i].overlaps(&claims[j]) {
+                overlaps = true;
+                break;
+            }
+        }
+
+        if !overlaps {
+            println!("part2: {}", &claims[i].id);
+            break;
+        }
+    }
 
     Ok(())
 }
