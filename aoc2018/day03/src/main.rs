c335379f1fd13f4e847d7f15af0b5275aa6676b7
Matthias Kestenholz <mk@feinheit.ch>
2023-02-03 21:22:24 +0100

    It's hard

--- aoc2018/day03/src/main.rs
+++ aoc2018/day03/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 // use std::iter::zip;
 
 #[derive(Copy, Clone)]
@@ -34,11 +35,13 @@ impl Rect {
         }
         let x = std::cmp::max(self.x, other.x);
         let y = std::cmp::max(self.y, other.y);
+        let w = std::cmp::min(self.x_outside(), other.x_outside()) - x;
+        let h = std::cmp::min(self.y_outside(), other.y_outside()) - y;
         Some(Rect {
             x,
             y,
-            w: std::cmp::min(self.x_outside(), other.x_outside()) - x,
-            h: std::cmp::min(self.y_outside(), other.y_outside()) - y,
+            w,
+            h,
             negative: true,
         })
     }
@@ -66,6 +69,7 @@ fn parse_claim(line: &str) -> Rect {
 }
 
 fn main() -> io::Result<()> {
+    // let file = fs::File::open("03-test.txt")?;
     let file = fs::File::open("03.txt")?;
     let reader = io::BufReader::new(file);
 
@@ -77,6 +81,7 @@ fn main() -> io::Result<()> {
 
     println!("claims: {}", claims.len());
 
+    /*
     let mut rects = Vec::new();
     for rect in claims {
         for i in 0..rects.len() {
@@ -95,6 +100,26 @@ fn main() -> io::Result<()> {
     }
 
     println!("area: {}", area);
+    */
+
+    let mut occupations: HashMap<i64, i64> = HashMap::new();
+    for claim in claims {
+        for dx in 0..claim.w {
+            for dy in 0..claim.h {
+                let v = (claim.y + dy) * 10000 + (claim.x + dx);
+                let n = occupations.get(&v).unwrap_or(&0);
+                occupations.insert(v, n + 1);
+            }
+        }
+    }
+    let mut more_than_one = 0;
+    for value in occupations.values() {
+        if value > &1 {
+            more_than_one += 1;
+        }
+    }
+
+    println!("part1: {}", more_than_one);
 
     Ok(())
 }
