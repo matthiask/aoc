cbe44bbe0a11b8f7e625f4ece4d7f2082783a646
Matthias Kestenholz <mk@feinheit.ch>
2023-02-04 19:55:27 +0100

    Man it's truly annoying

--- aoc2018/day04/src/main.rs
+++ aoc2018/day04/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::io::{self, BufRead};
 
+type Guard = Vec<i32>;
+
 fn main() -> io::Result<()> {
     // let file = fs::File::open("04-test.txt")?;
     let file = fs::File::open("04.txt")?;
@@ -11,8 +13,20 @@ fn main() -> io::Result<()> {
 
     println!("{}", lines.len());
 
+    let mut guards: Vec<Guard> = Vec::new();
+    let iter = lines.iter();
+
+    loop {
+        let mut guard: Guard = vec![0; 60];
+    }
+
+
     for line in &lines {
-        println!("{}", line);
+        // println!("{}", line);
+        let mut guard: Guard = vec![0; 60];
+
+        let parts: Vec<&str> = line.split(&[' ', '#']).collect();
+
     }
 
     Ok(())
