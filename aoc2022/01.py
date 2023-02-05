f3a0dfee430e734ea65431940e7a79cedc1be962
Matthias Kestenholz <mk@feinheit.ch>
2023-02-05 15:36:09 +0100

    Sum immediately

--- aoc2022/01.py
+++ aoc2022/01.py
@@ -1,14 +1,12 @@
-def read():
+def read_groups():
     groups = open("01.txt").read().strip().split("\n\n")
-    return [[int(line) for line in group.split("\n")] for group in groups]
+    return [sum(int(line) for line in group.split("\n")) for group in groups]
 
 
 def main():
-    groups = read()
-    carrying = [sum(group) for group in groups]
-    print(max(carrying))
-
-    print(sum(sorted(carrying, reverse=True)[:3]))
+    groups = read_groups()
+    print(max(groups))
+    print(sum(sorted(groups, reverse=True)[:3]))
 
 
 if __name__ == "__main__":
