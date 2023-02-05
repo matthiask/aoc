b9fb5f5d0e22267d94f23737696331b89dc4d103
Matthias Kestenholz <mk@feinheit.ch>
2023-02-05 21:31:53 +0100

    Some parsing

---
+++ aoc2018/day06/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::io::{self, BufRead};
+
+#[derive(Copy, Clone)]
+struct Coords {
+    x: i32,
+    y: i32,
+}
+
+fn coords_from_str(s: &str) -> Coords {
+    let pair = s.split_once(", ").unwrap();
+    let x = pair.0.parse::<i32>().unwrap();
+    let y = pair.1.parse::<i32>().unwrap();
+    Coords {
+        x,
+        y,
+    }
+}
+
+fn main() {
+    let file = fs::File::open("06-test.txt").unwrap();
+    // let file = fs::File::open("06.txt").unwrap();
+    let reader = io::BufReader::new(file);
+
+    let coords: Vec<Coords> = reader.lines().map(|line| coords_from_str(&line.unwrap())).collect();
+
+    println!("coords: {}", coords.len());
+}
