de257a71da2a9ed352c85e3f3129d1e11a4e0fe8
Matthias Kestenholz <mk@feinheit.ch>
2023-02-06 20:09:45 +0100

    Parsing

---
+++ aoc2018/day07/src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs;
+use std::io::{self, BufRead};
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct Before {
+    before: char,
+    after: char,
+}
+
+fn parse_before(s: &str) -> Before {
+    let tokens: Vec<&str> = s.split_whitespace().collect();
+    Before {
+        before: tokens[1].chars().next().unwrap(),
+        after: tokens[7].chars().next().unwrap(),
+    }
+}
+
+fn process(filename: &str) {
+    let file = fs::File::open(filename).unwrap();
+    let reader = io::BufReader::new(file);
+
+    let coords: Vec<Before> = reader
+        .lines()
+        .map(|line| parse_before(&line.unwrap()))
+        .collect();
+
+    println!("pairs: {}", coords.len());
+    println!("{:?}", &coords);
+}
+
+fn main() {
+    for filename in env::args().skip(1) {
+        process(&filename);
+    }
+}
