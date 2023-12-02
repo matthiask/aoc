b3afbe98a1d0cc376634ab9c3680734626ddc6b4
Matthias Kestenholz <mk@feinheit.ch>
2023-12-02 08:01:18 +0100

    Part 1

---
+++ aoc2023/02.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from pprint import pprint
+
+from tools import open_input
+
+
+@dataclass
+class Sample:
+    red: int = 0
+    green: int = 0
+    blue: int = 0
+
+    def __lt__(self, other):
+        return (
+            self.red <= other.red
+            and self.green <= other.green
+            and self.blue <= other.blue
+        )
+
+
+@dataclass
+class Game:
+    id: int
+    samples: list[Sample]
+
+
+IN = [*open_input("02")]
+
+
+def sample(sample):
+    cubes = sample.split(", ")
+    return Sample(**{cube.split()[1]: int(cube.split()[0]) for cube in cubes})
+
+
+def games(lines):
+    for line in lines:
+        game, samples = line.split(": ")
+        yield Game(
+            id=int(game.split()[1]), samples=[sample(s) for s in samples.split("; ")]
+        )
+
+
+def solve1():
+    counts = Sample(red=12, green=13, blue=14)
+    # pprint(list(games(IN)))
+
+    possible_id_sum = 0
+    for game in games(IN):
+        if all(sample < counts for sample in game.samples):
+            possible_id_sum += game.id
+
+    pprint(possible_id_sum)
+
+
+solve1()
