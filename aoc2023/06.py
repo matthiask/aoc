2c47026922b1d06e3fc23812ac2b3afa5cb2d359
Matthias Kestenholz <mk@feinheit.ch>
2023-12-06 08:27:39 +0100

    2023 06 part2

--- aoc2023/06.py
+++ aoc2023/06.py
@@ -3,23 +3,46 @@ from pprint import pprint
 from tools import numbers, open_input
 
 
-input = open_input("06")
-races = list(zip(numbers(next(input)), numbers(next(input))))
-
-
-pprint(races)
-
-
 def distance_mm(button_ms, race_ms):
     mm_ms = button_ms
     return mm_ms * (race_ms - button_ms)
 
 
-part1 = 1
-for race_ms, distance_to_beat in races:
-    part1 *= sum(
-        1 if distance_mm(button_ms, race_ms) > distance_to_beat else 0
-        for button_ms in range(1, race_ms)
+def solve1():
+    input = open_input("06")
+    races = list(zip(numbers(next(input)), numbers(next(input))))
+
+    pprint(races)
+
+    part1 = 1
+    for race_ms, distance_to_beat in races:
+        part1 *= sum(
+            1 if distance_mm(button_ms, race_ms) > distance_to_beat else 0
+            for button_ms in range(1, race_ms)
+        )
+
+    pprint(("part1", part1))
+
+
+def solve2():
+    input = open_input("06")
+    races = list(
+        zip(
+            numbers(next(input).replace(" ", "")), numbers(next(input).replace(" ", ""))
+        )
     )
 
-pprint(("part1", part1))
+    pprint(races)
+
+    part2 = 1
+    for race_ms, distance_to_beat in races:
+        part2 *= sum(
+            1 if distance_mm(button_ms, race_ms) > distance_to_beat else 0
+            for button_ms in range(1, race_ms)
+        )
+
+    pprint(("part2", part2))
+
+
+solve1()
+solve2()
