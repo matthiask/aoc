a54ec22e98c76c0a3fd749e3ea9e44fb327fdcf4
Matthias Kestenholz <mk@feinheit.ch>
2023-12-07 08:59:45 +0100

    2023 07 part2, good fun

--- aoc2023/07.py
+++ aoc2023/07.py
@@ -4,14 +4,48 @@ from pprint import pp
 from tools import open_input
 
 
-strengths = {
-    card: strength + 2 for strength, card in enumerate(reversed("AKQJT98765432"))
-}
+part2 = True
+
+if part2:  # noqa: SIM108
+    # Part2
+    cards = "AKQT98765432J"
+else:
+    # Part1
+    cards = "AKQJT98765432"
+
+strengths = {card: strength + 2 for strength, card in enumerate(reversed(cards))}
+
+
+def type_with_jokers(counts, groups):
+    # pp(counts)
+    if "J" not in counts:
+        return None
+
+    jokers = counts.pop("J")
+    groups = [c[1] for c in counts.most_common()]
+
+    if not groups:
+        return 10, "five"
+    if groups[0] == 5 - jokers:
+        return 10, "five"
+    if groups[0] == 4 - jokers:
+        return 8, "four"
+    if set(groups) == {3 - jokers, 2}:
+        return 6, "full house"
+    if groups[0] == 3 - jokers:
+        return 5, "three of a kind"
+    if set(groups[:2]) == {2, 2 - jokers}:
+        return 4, "two pair"
+    return 3, "one pair"
+    print(jokers, counts)
 
 
 def type(hand):
-    counts = Counter(hand).most_common()
-    groups = [c[1] for c in counts]
+    counts = Counter(hand)
+    groups = [c[1] for c in counts.most_common()]
+
+    if part2 and (with_jokers := type_with_jokers(counts, groups)):
+        return with_jokers
 
     if groups == [5]:
         return 10, "five"
@@ -59,7 +93,7 @@ def test():
         pp((hand, type(hand)))
 
 
-def solve1():
+def parse_hands_and_pids():
     hands_and_bids = {}
     for line in open_input("07"):
         parts = line.strip().split()
@@ -68,6 +102,11 @@ def solve1():
         if hand in hands_and_bids:
             raise Exception()
         hands_and_bids[hand] = bid
+    return hands_and_bids
+
+
+def solve1():
+    hands_and_bids = parse_hands_and_pids()
     pp(hands_and_bids)
 
     hands = sorted(
@@ -86,4 +125,23 @@ def solve1():
     )
 
 
-solve1()
+def solve2():
+    hands_and_bids = parse_hands_and_pids()
+    # pp(hands_and_bids)
+    pp({hand: type(hand) for hand in hands_and_bids})
+    hands = sorted(
+        hands_and_bids.keys(),
+        key=lambda hand: (type(hand)[0], hand_to_strengths(hand)),
+    )
+    pp(
+        (
+            "part2",
+            sum(hands_and_bids[hand] * (idx + 1) for idx, hand in enumerate(hands)),
+        )
+    )
+
+
+if part2:
+    solve2()
+else:
+    solve1()
