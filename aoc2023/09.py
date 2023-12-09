bd41712defe0a6d6fab5be2792f8be356be17ca5
Matthias Kestenholz <mk@feinheit.ch>
2023-12-09 09:02:15 +0100

    2023 09 part2

--- aoc2023/09.py
+++ aoc2023/09.py
@@ -13,16 +13,25 @@ def differences(line):
 
 def solve1():
     lines = parse()
-    total = 0
+    total_1 = 0
+    total_2 = 0
     for line in lines:
-        line_total = 0
+        line_total_1 = 0
+        line_total_2 = 0
+        sign = 1
         while any(line):
             # pp(line)
-            line_total += line[-1]
+            line_total_1 += line[-1]
+            line_total_2 += line[0] * sign
+            sign *= -1
             line = differences(line)
-        total += line_total
-        pp(("line_total", line, line_total))
-    pp(("part1", total))
+            # pp(locals())
+        total_1 += line_total_1
+        total_2 += line_total_2
+        pp(("line_total_1", line, line_total_1))
+        pp(("line_total_2", line, line_total_2))
+    pp(("part1", total_1))
+    pp(("part2", total_2))
 
     # pp(n)
 
