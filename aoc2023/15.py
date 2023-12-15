63ea6dd15d0c59860e432f6c0d5848acc5825a35
Matthias Kestenholz <mk@feinheit.ch>
2023-12-15 08:36:43 +0100

    Reading comprehension exercise

--- aoc2023/15.py
+++ aoc2023/15.py
@@ -17,3 +17,30 @@ def HASH(string):  # noqa: N802
 # pp([(s, HASH(s)) for s in IN.split(",")])
 
 pp(("part1", sum(map(HASH, IN.split(",")))))
+
+
+def solve2():
+    boxes = [{} for _ in range(256)]
+    for s in IN.split(","):
+        if s.endswith("-"):
+            label = s[:-1]
+            box = HASH(label)
+
+            boxes[box].pop(label, None)
+
+        else:
+            label, focal_length = s.split("=")
+            box = HASH(label)
+            focal_length = int(focal_length)
+
+            # Dicts are ordered :-)
+            boxes[box][label] = focal_length
+
+    focussing_power = 0
+    for box_i, box in enumerate(boxes):
+        for slot_i, focal_length in enumerate(box.values()):
+            focussing_power += (box_i + 1) * (slot_i + 1) * focal_length
+    pp(("part2", focussing_power))
+
+
+solve2()
