60f9b49dc3ddc9f8534efbf8e0bff9ac5f2d7c4e
Matthias Kestenholz <mk@feinheit.ch>
2023-12-21 07:26:07 +0100

    2023 21 part1

---
+++ aoc2023/21.py
@@ -0,0 +1,33 @@
+from itertools import chain
+from pprint import pp
+
+from tools import neighbors, open_input
+
+
+G = {
+    x + y * 1j: c
+    for y, line in enumerate(open_input("21").read().strip().split("\n"))
+    for x, c in enumerate(line)
+}
+S = next(xy for xy, c in G.items() if c == "S")
+# pp(G)
+# pp(S)
+
+
+def solve1():
+    places = {S}
+    for _ in range(64):
+        places = set(
+            chain.from_iterable(
+                (
+                    xy
+                    for xy in neighbors(prev, diagonal=False)
+                    if G.get(xy) in {".", "S"}
+                )
+                for prev in places
+            )
+        )
+    pp(("part1", len(places)))
+
+
+solve1()
