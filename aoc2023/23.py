c0bbb86aa13dc898596377138fa834d39d0928de
Matthias Kestenholz <mk@feinheit.ch>
2023-12-23 11:54:34 +0100

    I like grids

---
+++ aoc2023/23.py
@@ -0,0 +1,32 @@
+from pprint import pp
+
+from tools import open_input
+
+
+IN = [*open_input("23")]
+G = {x + y * 1j: c for y, line in enumerate(IN) for x, c in enumerate(line.strip())}
+start = next(xy for xy, c in G.items() if xy.imag == 0 and c == ".")
+end = next(xy for xy, c in G.items() if xy.imag == len(IN) - 1 and c == ".")
+# pp(G)
+# pp((start, end))
+
+neighbors_with_slopes = [(1, ">"), (1j, "v"), (-1, "<"), (-1j, "^")]
+
+paths = [[start]]
+final = []
+while paths:
+    next_paths = []
+    for path in paths:
+        for dxy, slope in neighbors_with_slopes:
+            if (
+                (next_xy := path[-1] + dxy)
+                and (c := G.get(next_xy))
+                and next_xy not in path
+            ):
+                if next_xy == end:
+                    final.append([*path, next_xy])
+                elif c == slope or c == ".":
+                    next_paths.append([*path, next_xy])
+    paths = next_paths
+
+pp([len(path) - 1 for path in final])
