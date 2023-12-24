a207eb1464003dbf28a8b35d8c18a7cd2f35687c
Matthias Kestenholz <mk@feinheit.ch>
2023-12-24 15:10:57 +0100

    Horrible

--- aoc2023/24.py
+++ aoc2023/24.py
@@ -14,6 +14,61 @@ class Vec:
     y: int
     z: int
 
+    @property
+    def xy2d(self):
+        return Vec2d(self.x, self.y)
+
+
+@dataclass
+class Mat2d:
+    values: list[list[int]]
+
+    @property
+    def det(self):
+        v = self.values
+        return v[0][0] * v[1][1] - v[0][1] * v[1][0]
+
+    @property
+    def inv(self):
+        f = 1 / self.det
+        v = self.values
+        return Mat2d(
+            [
+                [f * v[1][1], -f * v[0][1]],
+                [-f * v[1][0], f * v[0][0]],
+            ]
+        )
+
+    def __mul__(self, other):
+        if isinstance(other, Vec2d):
+            v = self.values
+            return Vec2d(
+                v[0][0] * other.x + v[0][1] * other.y,
+                v[1][0] * other.x + v[1][1] * other.y,
+            )
+        return NotImplemented
+
+
+@dataclass
+class Vec2d:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self.x + other.x, self.y + other.y)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Vec2d):
+            return Vec2d(self.x - other.x, self.y - other.y)
+        return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vec2d(self.x * other, self.y * other)
+        return NotImplemented
+
 
 @dataclass
 class Hailstone:
@@ -43,6 +98,38 @@ def solve1(inp, min, max):
         # (p1 - p2) / (v2 - v1) = t
         # pprint((h1, h2))
 
+        # Av = b
+        A = Mat2d(
+            [
+                [h1.v.x, -h2.v.x],
+                [h1.v.y, -h2.v.y],
+            ]
+        )
+        b = h2.p.xy2d - h1.p.xy2d
+
+        try:
+            v = A.inv * b
+        except ZeroDivisionError:
+            continue
+        else:
+            p = h1.p.xy2d + h1.v.xy2d * v.x
+            # print(h1)
+            # print(h2)
+            # print(A)
+            # print(A.inv)
+            # print(b)
+            # print(v, p)
+            # print()
+
+            if v.x > 0 and v.y > 0:
+                if min <= p.x <= max and min <= p.y <= max:
+                    intersections += 1
+                    # print("Intersects!")
+
+        """
+                print(x, p)
+        continue
+
         try:
             t = (h1.p.x - h2.p.x) / (h2.v.x - h1.v.x)
         except ZeroDivisionError:
@@ -52,6 +139,8 @@ def solve1(inp, min, max):
             if min <= at.x <= max and min <= at.y <= max:
                 # print(t, at)
                 intersections += 1
+        """
+
     print(intersections)
 
 
