7faac0f69aa41f602ebb348e42ad4ab71d568c93
Matthias Kestenholz <mk@feinheit.ch>
2023-12-24 13:22:15 +0100

    lol

---
+++ aoc2023/24.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from itertools import combinations
+
+from tools import numbers, open_input
+
+
+def almost_equal(f1, f2):
+    return abs(f1 - f2) < 0.0001
+
+
+@dataclass
+class Vec:
+    x: int
+    y: int
+    z: int
+
+
+@dataclass
+class Hailstone:
+    p: Vec
+    v: Vec
+
+    @classmethod
+    def from_line(cls, line):
+        n = numbers(line)
+        return cls(Vec(*n[:3]), Vec(*n[3:]))
+
+    def at(self, t):
+        return Vec(
+            self.p.x + t * self.v.x,
+            self.p.y + t * self.v.y,
+            self.p.z + t * self.v.z,
+        )
+
+
+def solve1(inp, min, max):
+    stones = [Hailstone.from_line(line) for line in open_input(inp)]
+    intersections = 0
+    for h1, h2 in combinations(stones, 2):
+        # p1 + t * v1 = p2 + t * v2
+        # p1 - p2 = t * (v2 - v1)
+        # (p1 - p2) / (v2 - v1) = t
+        # pprint((h1, h2))
+
+        try:
+            t = (h1.p.x - h2.p.x) / (h2.v.x - h1.v.x)
+        except ZeroDivisionError:
+            continue
+        if t > 0:
+            at = h1.at(t)
+            if min <= at.x <= max and min <= at.y <= max:
+                print(t, at)
+                intersections += 1
+    print(intersections)
+
+
+solve1("24-test", 7, 27)
+solve1("24", 200000000000000, 400000000000000)
